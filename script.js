4b7e176272d757ef18b51c35ca81a72b44f62a02
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-10 19:40:51 +0100

    feat: slider funzionante

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 'use strict';
 
-/* 
-      <div class="slide active">
-        <img src="img/01.jpg" alt="immagine 1">
-      </div>
-      <div class="slide">
-        <img src="img/02.jpg" alt="immagine 2">
-      </div>
-      <div class="slide">
-        <img src="3" alt="immagine 3">
-      </div>
-      <div class="slide active">
-        <img src="img/04.jpg" alt="immagine 4">
-      </div> */
-
 const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg'];
 const slider = document.querySelector('.slider');
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+
 let currentSlide = 0
+
+//Creazione markup html
 
 for (let i = 0; i < images.length; i++) {
   
-  //slide
+  //Creazione slide
   const slide = document.createElement('div');
   slide.classList.add('slide');
 
@@ -29,7 +20,7 @@ for (let i = 0; i < images.length; i++) {
   }
 
 
-  //img
+  //Aggiunta immagini
   const img = document.createElement('img');
   img.src = `img/${images[i]}`;
   img.alt = `Immagine Suggestiva ${i}`;
@@ -37,3 +28,41 @@ for (let i = 0; i < images.length; i++) {
   slide.append(img);
   slider.append(slide);
 }
+
+
+const domSlides = document.querySelectorAll('.slide');
+
+
+//Eventi che manipolano lo slider
+
+
+//Tasto destro
+left.addEventListener('click', function () {
+  console.log('ho cliccato su left');
+
+
+  //Se la slide selezionata dell'array ha una posizione maggiore di 0, rimuovo la classe active da questa, mi sposto di una slide e poi aggiungo la classe active
+  if(currentSlide > 0) {
+    
+    domSlides[currentSlide].classList.remove('active');
+
+    currentSlide--;
+    
+    domSlides[currentSlide].classList.add('active');
+  }
+});
+
+//Tasto sinistro
+right.addEventListener('click', function () {
+  console.log('ho cliccato su right');
+
+  //Se la slide selezionata dell'array ha una posizione minore della lunghezza totale dell'array -1, rimuovo la classe active da questa, mi sposto di una slide e poi aggiungo la classe active
+  if(currentSlide < domSlides.length - 1) {
+
+    domSlides[currentSlide].classList.remove('active');
+
+    currentSlide++;
+    
+    domSlides[currentSlide].classList.add('active');
+  }
+});
